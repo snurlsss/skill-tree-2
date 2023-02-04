8b9c1fe91fc8e1dd7571f2253eb1b8271456c0a8
Jojogrec <johannes.cederberg.4471@student.uu.se>
2023-02-04 14:14:23 +0100

    Solved errors with Waypoints.cs
    Solved compilation errors in Waypoints.cs.

--- Assets/Scripts/Waypoints.cs
+++ Assets/Scripts/Waypoints.cs
@@ -6,8 +6,8 @@ public class Waypoints : MonoBehaviour{
     public static Transform[] points;
 
     void Awake(){
-        points = Transform[transform.childCount];
-        for(int i = 0; i < length; i++){
+        for(int i = 0; i < transform.childCount; i++)
+        {
             points[i] = transform.GetChild(i);
         }
     }
